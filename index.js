88ef1cf55515b734ac1c7276ebc54f5e60870509
Fahad <35401837+fahadalgharazi@users.noreply.github.com>
2023-10-22 16:38:40 -0400

    fi

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // dependencies
 const express = require("express");
 const app = express();
-const port = 8000;
 const mime = require("mime-types");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const mongo = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken"); // auth tokens: https://jwt.io/introduction
+
+//port
+const port = 8000;
 
 // connect to mongo
 // useNewUrlParser: uses newer parser instead of legacy one
@@ -15,7 +18,6 @@ mongo.connect("mongodb://mongo:27017/312Not-Local", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const user_collection = mongo.Collection("Users");
 
 // creates schema object
 const Schema = mongo.Schema;
@@ -29,20 +31,36 @@ const user_schema = new Schema({
   email: String,
   password: String,
 });
-
+const auth_schema = new Schema({
+  auth_key: String,
+  creation_date: {
+    type: Date,
+    default: Date.now,
+  },
+  username: String,
+});
+// creates a model, which is basically db["users"]
+const User = mongo.model("users", user_schema);
+const Auth = mongo.model("Auth", auth_schema);
+//
+function escapeHTML(unsafeText) {
+  return unsafeText
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
 // DATABASE CRUD
-async function add_new_user(username, password, email) {
+async function add_new_user(username, password) {
   // async and await allow other processes to run while this is running
-  // creates a model, which is basically db["User"]
-  const User = mongoose.model("Users", user_schema);
-
   // create a new document for user
+  const esc_user = escapeHTML(username);
   const new_user = new User({
-    name: username,
-    email: email,
+    name: esc_user,
+    //email: email,
     password: password,
   });
-
   // hash pw
   new_user.password = await bcrypt.hash(password, 10);
 
@@ -51,14 +69,49 @@ async function add_new_user(username, password, email) {
     .save() // can use save() or insertOne() but save() is more convenient
     .then(() => console.log("User saved: ", new_user["name"]))
     .catch((error) => console.error(error));
+  console.log("Registering: ", esc_user);
 }
 
 async function verify_user(username, password) {
-  const fetched_data = user_collection.findOne({ name: username });
-  if (fetched_data) {
-    // if not none check if hashes of passwords match
+  const esc_user = escapeHTML(username);
+  try {
+    const fetched_data = await User.findOne({ name: esc_user });
+    if (fetched_data) {
+      const is_match = await bcrypt.compare(password, fetched_data.password); // returns a promise, await is needed for bool val
+      if (is_match) {
+        // this block of code checks if passwords match, if yes, send an auth token to client
+        const metadata = {
+          // metadata about token
+          type: "user",
+          name: esc_user,
+        };
+        const secret_key = "secret_key_to_sign_for_jwt";
+        const auth_token = jwt.sign(metadata, secret_key, { expiresIn: "1h" });
+        const auth_entry = new Auth({
+          username: esc_user,
+          auth_key: auth_token,
+        });
+        await auth_entry.save(); // save info into database
+        return auth_token;
+      } else {
+        return "Invalid password!";
+      }
+    } else {
+      return "User not found!";
+    }
+  } catch (error) {
+    // catch whatever error for debuggings
+    console.error(error);
+    return "An error occurred while verifying the user.";
+  }
+}
+
+async function token_checker(token) {
+  const doc = Auth.findOne({ auth_key: token });
+  if (doc) {
+    return doc["username"];
   } else {
-    res.send();
+    return "";
   }
 }
 
@@ -75,6 +128,8 @@ const setHeaders = function (req, res, next) {
 app.use(setHeaders);
 app.use(cookieParser());
 app.use("/public", express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // http requests
 app.get("/visit-counter", (req, res) => {
@@ -88,17 +143,63 @@ app.get("/visit-counter", (req, res) => {
   }
   res.sendFile(path.join(__dirname, "public", "visit-counter.html"));
 });
-
+//gets
 app.get("/", (req, res) => {
   const filePath = path.join(__dirname, "public", req.path);
   res.sendFile(filePath);
 });
 
-// posts
-app.post("/register", (req, res) => {
-  // check if username exists
-  console.log("Registering!");
+app.get("/user_check", (req, res) => {
+  const token_cookie = req.cookies("token_cookie");
+  res.send(token_checker(token_cookie));
 });
+
+// posts
+app.post("/register", async (req, res) => {
+  const name = req.body.username_reg;
+  const password = req.body.password_reg;
+  if (!name || !password) {
+    return res.status(400).send("All fields are required!");
+  }
+  try {
+    const user_document = await User.findOne({ name: name }); // await the database fetch
+    if (user_document) {
+      return res.status(400).send("Username already exists!"); // if user exists, throw this err
+    }
+    await add_new_user(name, password); // idk if await should be there
+    res.send("User registered successfully!");
+  } catch (error) {
+    console.error("Error occurred:", error); // log error for debugging
+    res.status(500).send("A server error has occurred: " + String(error));
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const user_login = escapeHTML(req.body.username_login);
+  const pass_login = req.body.password_login;
+  const user_verification = await verify_user(user_login, pass_login);
+  console.log(user_verification);
+  if (
+    user_verification !== "User not found!" &&
+    user_verification !== "Invalid password!"
+  ) {
+    res
+      .cookie("token_cookie", user_verification, {
+        maxAge: 3600000, // one hr in ms
+        httpOnly: true,
+        sameSite: "strict", // no cross-site cookie viewing
+      })
+      .cookie("username", user_login, {
+        // dont set httponly, so it is readable by the client
+        maxAge: 3600000, // one hour in milliseconds
+        sameSite: "strict",
+      })
+      .send("Login successful!\n" + "Logged in: " + user_login);
+  } else {
+    res.status(401).send(user_verification);
+  }
+});
+
 // running the app
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- public/functions.js
+++ public/functions.js
@@ -1,21 +1,82 @@
-function simpleButton(){
-    var prophecy = ""
-    if (confirm("Choose the Prophecy")){
-        prophecy = "He was the Chosen One"
+function cookie_fetch(name) {
+  let cookies = document.cookie.split("; "); // doc cookie returns {cookiename=cookie; cookiename2=cookie2; ...}
+  for (let cookie of cookies) {
+    let sc = cookie.split("=");
+    if (sc[0].startsWith(name)) {
+      return sc[1];
     }
-    document.getElementById("paragraph").innerHTML = prophecy;
+  }
+  return null;
 }
 
-function cookie(){
-    visit = document.cookie
-    visVal = 0;
-    console.log(visit)
-    if (document.cookie = "") {
-        visVal = 1
-    }
-    else {
-        getVisit = visit.split('=')
-        visVal = parseInt(getVisit[1])
-    }
-    document.getElementById("paragraph").innerHTML += visVal;
+function simpleButton() {
+  var prophecy = "";
+  if (confirm("Choose the Prophecy")) {
+    prophecy = "He was the Chosen One";
+  }
+  document.getElementById("paragraph").innerHTML = prophecy;
+}
+
+function display_username() {
+  const display = document.getElementById("display_name");
+  let username = cookie_fetch("username");
+  if (username) {
+    display.innerHTML = display.innerText + `<strong> ${username} </strong>`;
+  }
+}
+function cookie() {
+  visit = document.cookie;
+  visVal = 0;
+  console.log(visit);
+  if ((document.cookie = "")) {
+    visVal = 1;
+  } else {
+    getVisit = visit.split("=");
+    visVal = parseInt(getVisit[1]);
+  }
+  document.getElementById("paragraph").innerHTML += visVal;
+}
+
+postJSON = {
+  username: "fahad",
+  descript: "Hello World again",
+  id: "1738",
+  title: "first post",
+};
+
+document.addEventListener("DOMContentLoaded", functionCall());
+function functionCall() {
+  addMessageToChat(postJSON);
+}
+
+function postMessageHTML(postJSON) {
+  const username = postJSON.username;
+  const title = postJSON.title;
+  const descript = postJSON.descript;
+  const id = postJSON.id;
+  let messageHTML =
+    "<div  id='message_" +
+    id +
+    "' class='card'>\
+        <div class='container'>\
+          <h2><b>" +
+    title +
+    "</b></h4> \
+          <h4>" +
+    username +
+    "</h4>\
+          <p> " +
+    descript +
+    "</p> \
+        </div>\
+      </div>";
+  return messageHTML;
+}
+
+function addMessageToChat(postJSON) {
+  const chatMessages = document.getElementById("chat-messages");
+  chatMessages.innerHTML += postMessageHTML(postJSON);
+  chatMessages.scrollIntoView(false);
+  chatMessages.scrollTop =
+    chatMessages.scrollHeight - chatMessages.clientHeight;
 }
