6b5fc944b3c81e0911955f711f83bc0f07baa6ed
brandon pc <brandonqiu777@gmail.com>
2023-11-10 17:13:17 -0500

    page fills out, added styling

--- index.js
+++ index.js
@@ -33,18 +33,6 @@ const user_schema = new Schema({
   password: String,
 });
 
-// Schema for posts
-const post_schema = new Schema({
-  user: String,
-  creation_date: {
-    type: Date,
-    default: Date.now,
-  },
-  title: String,
-  description: String,
-  users_liked: [],
-  liked: Boolean,
-});
 const auth_schema = new Schema({
   auth_key: String,
   creation_date: {
@@ -61,21 +49,18 @@ const auction_schema = new Schema({
     type: Date,
     default: Date.now,
   },
-  owner: String,
+  seller: String,
   description: String,
-  current_price: Number,
-  price_history: {},
+  current_bid: [String, Number],
+  price_history: { String: (String, Number) }, // (date, (bidder,price))
   id: String,
 });
 // creates a model, which is basically db["users"]
 const User = mongo.model("users", user_schema);
 
-// db["Post"]
-const Post = mongo.model("Post", post_schema);
-
 const Auth = mongo.model("Auth", auth_schema);
 //
-const Auctions = mongo.model("Auctions", auction_schema);
+const Auctions = mongo.model("Auction", auction_schema);
 function escapeHTML(unsafeText) {
   return unsafeText
     .replace(/&/g, "&amp;")
@@ -86,7 +71,7 @@ function escapeHTML(unsafeText) {
 }
 // DATABASE CRUD
 async function add_new_auction(
-  user,
+  seller,
   item,
   start_price,
   description,
@@ -94,11 +79,11 @@ async function add_new_auction(
 ) {
   try {
     const new_auction = new Auctions({
-      owner: escapeHTML(user),
+      seller: escapeHTML(seller),
       image_path: image_path,
       item_name: escapeHTML(item),
       description: escapeHTML(description),
-      current_price: start_price,
+      current_bid: ("Start Price", start_price),
       price_history: { startprice: (start_price, Date.now().toString) },
       id: (Math.random() * 1000000000).toString, // might still have conflicts if unlucky enough, change this to jwt token for guarenteed uniqueness
     });
@@ -133,91 +118,6 @@ async function add_new_user(username, password) {
     .then(() => console.log("User saved: ", new_user["name"]))
     .catch((error) => console.error(error));
   console.log("Registering: ", esc_user);
-}
-
-// async function add_new_post(username, title, description) {
-//   // async and await allow other processes to run while this is running
-//   // create a new document for post
-//   const esc_title = escapeHTML(title);
-//   const esc_desc = escapeHTML(description);
-//   const new_post = new Post({
-//     user: username,
-//     title: esc_title,
-//     description: esc_desc,
-//   });
-//   // save it to database
-//   await new_post
-//     .save() // can use save() or insertOne() but save() is more convenient
-//     .then(() => console.log("Post Made: ", new_post["user"]))
-//     .catch((error) => console.error(error));
-// }
-
-async function add_new_post(username, title, description) {
-  // async and await allow other processes to run while this is running
-  // create a new document for post
-  const esc_title = escapeHTML(title);
-  const esc_desc = escapeHTML(description);
-  const esc_user = escapeHTML(username);
-  const new_post = new Post({
-    user: esc_user,
-    title: esc_title,
-    description: esc_desc,
-    users_liked: [],
-    liked: false,
-  });
-  // save it to database
-  await new_post
-    .save() // can use save() or insertOne() but save() is more convenient
-    .then(() => console.log("Post Made: ", new_post["user"]))
-    .catch((error) => console.error(error));
-}
-
-async function verify_user(username, password) {
-  const esc_user = escapeHTML(username);
-  try {
-    const fetched_data = await User.findOne({ name: esc_user });
-    if (fetched_data) {
-      const is_match = await bcrypt.compare(password, fetched_data.password); // returns a promise, await is needed for bool val
-      if (is_match) {
-        // this block of code checks if passwords match, if yes, send an auth token to client
-        const metadata = {
-          // metadata about token
-          type: "user",
-          name: esc_user,
-        };
-        const secret_key = "secret_key_to_sign_for_jwt";
-        const auth_token = jwt.sign(metadata, secret_key, { expiresIn: "1h" });
-        const auth_entry = new Auth({
-          username: esc_user,
-          auth_key: auth_token,
-        });
-        await auth_entry.save(); // save info into database
-        return auth_token;
-      } else {
-        return "Invalid password!";
-      }
-    } else {
-      return "User not found!";
-    }
-  } catch (error) {
-    // catch whatever error for debuggings
-    console.error(error);
-    return "An error occurred while verifying the user.";
-  }
-}
-
-async function token_checker(token) {
-  const doc = Auth.findOne({ auth_key: token });
-  if (doc) {
-    return doc["username"];
-  } else {
-    return "";
-  }
-}
-async function getAllPosts() {
-  const posts = await Post.find({});
-  const jString = JSON.stringify(posts);
-  return posts;
 }
 // middlewares
 const setHeaders = function (req, res, next) {
@@ -280,14 +180,21 @@ app.get("/update-feed", (req, res) => {
 
 app.get("/auction-page", async (req, res) => {
   // serve the html
-  // const id = req.query;
+
   // const auction_data = await Auctions.findOne({ id: id });
   const auction_page_path = path.join(__dirname, "public", "auction_page.html");
   res.sendFile(auction_page_path);
 });
 
 app.get("/get-auction-data", async (req, res) => {
-  const auction_data = Auctions.findOne({ id: JSON.parse(req.body) });
+  const queries = req.query;
+  //console.log("QUERIES", queries);
+  let id = queries["id"];
+  //console.log("id in indexjs", id);
+  const auction_data = await Auctions.findOne({ id: id });
+  console.log("Auction data", auction_data);
+  //console.log("curr_bid", auction_data["current_bid"]);
+
   res.send(JSON.stringify(auction_data));
 });
 
@@ -300,6 +207,7 @@ app.post("/register", async (req, res) => {
   }
   try {
     const user_document = await User.findOne({ name: name }); // await the database fetch
+    //console.log("User document", user_document);
     if (user_document) {
       return res.status(400).send("Username already exists!"); // if user exists, throw this err
     }
--- public/functions.js
+++ public/functions.js
@@ -9,32 +9,12 @@ function cookie_fetch(name) {
   return null;
 }
 
-function simpleButton() {
-  var prophecy = "";
-  if (confirm("Choose the Prophecy")) {
-    prophecy = "He was the Chosen One";
-  }
-  document.getElementById("paragraph").innerHTML = prophecy;
-}
-
 function display_username() {
   const display = document.getElementById("display_name");
   let username = cookie_fetch("username");
   if (username) {
     display.innerHTML = display.innerText + `<strong> ${username} </strong>`;
   }
-}
-function cookie() {
-  visit = document.cookie;
-  visVal = 0;
-  console.log(visit);
-  if ((document.cookie = "")) {
-    visVal = 1;
-  } else {
-    getVisit = visit.split("=");
-    visVal = parseInt(getVisit[1]);
-  }
-  document.getElementById("paragraph").innerHTML += visVal;
 }
 
 function redirectCreateAuction() {
@@ -49,109 +29,7 @@ function redirectMyAuctions() {
   window.location.href = "/public/auction_page.html"; // Replace with your desired URL
 }
 
-// function chatMessageHTML(messageJSON) {
-//     const username = messageJSON.user;
-//     const message = messageJSON.title;
-//     const messageId = messageJSON.description;
-//     console.log(messageId)
-//     let messageHTML = "<br><button onclick='deleteMessage(\"" + messageId + "\")'>X</button> ";
-//     messageHTML += "<span id='message_" + messageId + "'><b>" + username + "</b>: " + message + "</span>";
-//     return messageHTML;
-// }
-
-function clearChat() {
-  const chatMessages = document.getElementById("chat-messages");
-  chatMessages.innerHTML = "";
-}
-
-function addMessageToChat(messageJSON) {
-  const chatMessages = document.getElementById("chat-messages");
-  chatMessages.innerHTML += postMessageHTML(messageJSON);
-  chatMessages.scrollIntoView(false);
-  chatMessages.scrollTop =
-    chatMessages.scrollHeight - chatMessages.clientHeight;
-}
-
-function makePost() {
-  const titleTextBox = document.getElementById("title-text-box");
-  const descBox = document.getElementById("description-text-box");
-  const title = titleTextBox.value;
-  const description = descBox.value;
-
-  titleTextBox.value = "";
-  descBox.value = "";
-
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      console.log(this.response);
-    }
-  };
-  const messageJSON = { title: title, description: description };
-  request.open("POST", "/make-post");
-  request.setRequestHeader("Content-Type", "application/json");
-  request.send(JSON.stringify(messageJSON));
-  titleTextBox.focus();
-  descBox.focus();
-}
-
-function updateFeed() {
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      clearChat();
-      const messages = JSON.parse(this.response);
-      // console.log(messages)
-      // console.log(JSON.stringify(messages))
-      for (const message of messages) {
-        addMessageToChat(message);
-      }
-    }
-  };
-  request.open("GET", "/update-feed");
-  request.send();
-}
-
-/*function welcome() {
-  document.addEventListener("keypress", function (event) {
-    if (event.code === "Enter") {
-      makePost();
-    }
-  });
-
-  document.getElementById("paragraph").innerHTML +=
-    "<br/>This text was added by JavaScript 😀";
-  document.getElementById("title-text-box").focus();
-  document.getElementById("description-text-box").focus();
-
-  updateFeed();
-  setInterval(updateFeed, 2000);
-}*/
-
-function welcome() {
-  /*document.addEventListener("keypress", function (event) {
-      if (event.code === "Enter") {
-          sendChat();
-      }
-  });*/
-
-
-  //document.getElementById("paragraph").innerHTML += "<br/>This text was added by JavaScript 😀";
-  //document.getElementById("chat-text-box").focus();
-
-  updateAuctions();
-
-  /*if (ws) {
-      initWS();
-  } else {
-      const videoElem = document.getElementsByClassName('video-chat')[0];
-      videoElem.parentElement.removeChild(videoElem);
-      setInterval(updateChat, 2000);
-  }*/
-
-  // use this line to start your video without having to click a button. Helpful for debugging
-  // startVideo();
-}
+updateAuctions();
 
 function display_username() {
   const display = document.getElementById("display_name");
@@ -165,135 +43,48 @@ function display_username() {
     }
   });
 
-  document.getElementById("title-text-box").focus();
-  document.getElementById("description-text-box").focus();
-
   updateFeed();
   setInterval(updateFeed, 2000);
 }
 
-function postMessageHTML(messageJSON) {
-  let messageHTML = "";
-  console.log(messageJSON);
-  const username = messageJSON.user;
-  const title = messageJSON.title;
-  const descript = messageJSON.description;
-  const id = messageJSON._id;
-  let liked = null;
-  let loggedUser = "";
-  const likeNum = messageJSON.users_liked.length;
-
+async function display_auction() {
+  // get id from url
+  let url = window.location.search;
+  const urlParams = new URLSearchParams(url);
+  const id = urlParams.get("id");
+  const get_url = "/get-auction-data?id=" + id;
   const request = new XMLHttpRequest();
   request.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-      const userCheck = JSON.parse(this.response);
-      loggedUser = userCheck["username"];
-      console.log("current user: " + loggedUser);
-      createMessageHTML();
-    }
-  };
-  request.open("GET", "/user_check");
-  request.send();
-
-  function createMessageHTML() {
-    if (messageJSON.users_liked.includes(loggedUser)) {
-      liked = true;
-      console.log(username + " already liked");
-    } else {
-      console.log(username + " has not liked");
-      liked = false;
-    }
-    console.log(messageJSON.users_liked);
-
-    if (liked == false) {
-      messageHTML = `<div id='message_${id}' class='card'>\
-          <div class='container'>\
-            <h2><b>${title}</b></h4> \
-            <h4>${username}</h4>\
-            <p>${descript}</p> \
-            <button id='likeBtn_${id}' onclick='likes("${id}")'>LIKE</button>\
-            <p id='likes_${id}'>Number of Likes: ${likeNum}</p>\
-          </div>\
-        </div>`;
-    } else {
-      messageHTML = `<div id='message_${id}' class='card'>\
-        <div class='container'>\
-          <h2><b>${title}</b></h4> \
-          <h4>${username}</h4>\
-          <p>${descript}</p> \
-          <button id='likeBtn_${id}' onclick='likes("${id}")'>UNLIKE</button>\
-          <p id='likes_${id}'>Number of Likes: ${likeNum}</p>\
-        </div>\
-      </div>`;
-    }
-
-    // Insert the message HTML into the chat
-    // For example:
-    const chatMessages = document.getElementById("chat-messages");
-    chatMessages.innerHTML += messageHTML;
-    chatMessages.scrollIntoView(false);
-    chatMessages.scrollTop =
-      chatMessages.scrollHeight - chatMessages.clientHeight;
-  }
-
-  return messageHTML;
-}
-
-function likes(id) {
-  const likeBtn = document.getElementById(`likeBtn_${id}`);
-  const likesElement = document.getElementById(`likes_${id}`);
-  let likes = parseInt(likesElement.innerText.split(":")[1]);
-  if (likeBtn.innerText == "LIKE") {
-    likes++;
-    likeBtn.innerText = "UNLIKE";
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        console.log(this.response);
-      }
-    };
-    const messageJSON = { likeStatus: true, likeId: id };
-    request.open("POST", "/like");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify(messageJSON));
-  } else {
-    likes--;
-    likeBtn.innerText = "LIKE";
-    const request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        console.log(this.response);
-      }
-    };
-    const messageJSON = { likeStatus: false, likeId: id };
-    request.open("POST", "/unlike");
-    request.setRequestHeader("Content-Type", "application/json");
-    request.send(JSON.stringify(messageJSON));
-  }
-
-  likesElement.innerText = `Number of Likes: ${likes}`;
-}
-
-function load_auction() {
-  let url = new URL(window.location.href);
-  let params = url.searchParams();
-  let id = params.get("id");
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
+      //console.log("Response " + request.responseText + " received from id");
       const auction_data = JSON.parse(this.responseText);
       const item_name = document.getElementById("item_header");
       const image = document
         .getElementById("auction_page_item")
-        .getElementsByTagName("img");
-      const desc = document.getElementById("item_info");
+        .getElementsByTagName("img")[0];
+      //console.log("Image!", image);
+      const desc = document.getElementById("desc");
       const owner = document.getElementById("item_owner");
+      let creation_date = document.getElementById("item_creation_date");
+      let bid_div = document.getElementById("bid");
+      let price = bid_div.getElementsByTagName("h2")[0];
+
+      let curr_bid = auction_data["current_bid"];
+      let bidder = curr_bid[0];
+      let amount = curr_bid[1];
       item_name.innerText = auction_data["item_name"];
-      image.src = auction_data["image_path"];
-      desc.innerText = auction_data["description"];
-      owner.innerText = auction_data["owner"];
+      image.src = "public/" + auction_data["image_path"];
+      desc.innerHTML =
+        "<strong>Description:</strong>\n" + auction_data["description"];
+      owner.innerText += "Seller: " + auction_data["seller"];
+      creation_date.innerText += " " + auction_data["creation_date"];
+      price.innerText += " $" + amount + ", " + bidder;
     }
   };
-  request.open("GET", "get-auction-data");
-  request.send(JSON.stringify(id));
+  request.open("GET", get_url);
+  request.send();
+}
+
+function welcome() {
+  console.log("HELLO");
 }
