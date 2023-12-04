00d59607ae0918f6678be5d5265b31724c510d1d
Dan <drboyle2@buffalo.edu>
2023-12-04 00:11:11 -0500

    changed links from local host to domain

--- public/userAuctions.js
+++ public/userAuctions.js
@@ -172,18 +172,18 @@
     return messageHTML;
   }
   function redirectCreateAuction() {
-    window.location.href = "/public/create_auction.html"; // Replace with your desired URL
+    window.location.href = "https://notlocal.live/public/create_auction.html"; // Replace with your desired URL
   }
   
   function redirectActiveAuctions() {
-    window.location.href = "/public/active_listings.html"; // Replace with your desired URL
+    window.location.href = "https://notlocal.live/public/active_listings.html"; // Replace with your desired URL
   }
   
   function redirectMyAuctionsCreated() {
-    window.location.href = "/auctionsCreated"; // Replace with your desired URL
+    window.location.href = "https://notlocal.live/auctionsCreated"; // Replace with your desired URL
   }
   function redirectMyAuctionsWon() {
-    window.location.href = "/auctionsWon"; // Replace with your desired URL
+    window.location.href = "https://notlocal.live/auctionsWon"; // Replace with your desired URL
   }
   
   function load_auction() {
